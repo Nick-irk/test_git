0fce520a6df29b85afa9894103129c698e507789
Nick Amin <58939316+Nick-irk@users.noreply.github.com>
2022-10-27 20:05:01 +0300

    Update TestClass.java

--- src/main/TestClass.java
+++ src/main/TestClass.java
@@ -3,7 +3,8 @@ package main;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("Проверка!");
+        System.out.println("Ещё одна проверка!");
         System.out.println("Второй коммит!");
+        System.out.println("Третий коммит!");
     }
 }
